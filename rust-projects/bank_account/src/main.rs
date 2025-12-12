1cf7bc44e31eb62037cd8b3203d3f7472f1e10d2
Pauls Toma <tomapt@udmercy.edu>
2025-12-12 00:07:00 -0500

    commit

--- rust-projects/bank_account/src/main.rs
+++ rust-projects/bank_account/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+struct BankAccount{
+    balance: f64,
 }
+
+impl BankAccount{
+    fn deposit(&mut self, amount: f64){
+        self.balance = self.balance + amount;
+    }
+}
+
+impl BankAccount{
+    fn withdraw(&mut self, amount: f64){
+        self.balance = self.balance - amount;
+    }
+}
+
+fn main(){
+    let mut account = BankAccount{
+        balance: 100.0,
+    };
+    
+    account.deposit(150.0);
+    account.withdraw(33.0);
+}
+
+
