ca050043957c73fdc7925ddf07a0254e6125f0dc
Pauls Toma <tomapt@udmercy.edu>
2025-12-22 20:19:14 -0500

    commit

--- rust-projects/bank_account/src/main.rs
+++ rust-projects/bank_account/src/main.rs
@@ -3,29 +3,23 @@ struct BankAccount{
 }
 
 impl BankAccount{
-    fn deposit(&mut self, amount: f64){
-        self.balance = self.balance + amount;
-    }
-}
-
-impl BankAccount{
     fn withdraw(&mut self, amount: f64){
         if amount > self.balance{
             println!("Error: withdrawal amount exceeds available balance.");
             return;
         }
-        self.balance = self.balance - amount;
-        }   
+        self.balance -= amount
     }
-
-fn main(){
-    let mut account = BankAccount{
-        balance: 0.0,
-    };
-
-    account.deposit(15.0);
-    account.withdraw(13.0);
-    println!("current balance in account: {}", account.balance);
+    
+    fn deposit(&mut self, amount: f64){
+        self.balance += amount;
+    }
 }
 
+fn main(){
+    let mut account = BankAccount{balance: 0.0,};
+    account.deposit(15.0);
+    account.withdraw(5.0);
 
+    println!("current balance in account: {}", account.balance);
+}
